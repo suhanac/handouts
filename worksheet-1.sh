269c711b4d166d6d8a5edc9ecb0804e783a4c0e8
suhanac <suhanac@umd.edu>
2017-08-01 13:22:35 -0400

    configure

--- worksheet-1.sh
+++ worksheet-1.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## Configure git
-git config --global user.name ...
-git config --global user.email ...
+system('git config --global user.name suhanac')
+system('git config --global user.email suhanac@umd.edu')
 
 ## Change the "origin" remote URL
-git remote set-url origin ...
+#git remote set-url origin ...
 
 ## Set the SESYNC-CI repository upstream and pull updates
-git remote add upstream https://github.com/sesync-ci/handouts.git
-git pull upstream master
+#git remote add upstream https://github.com/sesync-ci/handouts.git
+#git pull upstream master
